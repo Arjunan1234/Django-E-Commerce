7652de3f32c34373448d52694f23192a10dd49fb
esear <arjunanvijay21@gmail.com>
2024-02-02 17:49:29 +0530

    created gitignore file

--- store/urls.py
+++ store/urls.py
@@ -1,3 +1,4 @@
+from store.forms import LoginForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
@@ -8,7 +9,24 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.home, name="home"),
-      # URL for Authentication
+    # URL for Cart and Checkout
+    path('add-to-cart/', views.add_to_cart, name="add-to-cart"),
+    path('remove-cart/<int:cart_id>/', views.remove_cart, name="remove-cart"),
+    path('plus-cart/<int:cart_id>/', views.plus_cart, name="plus-cart"),
+    path('minus-cart/<int:cart_id>/', views.minus_cart, name="minus-cart"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('checkout/', views.checkout, name="orders"),
+    path('orders/', views.orders, name="orders"),
+
+    #URL for Products
+    path('product/<slug:slug>/', views.detail, name="product-detail"),
+    path('categories/', views.all_categories, name="all-categories"),
+    path('<slug:slug>/', views.category_products, name="category-products"),
+
+   
+
+    # URL for Authentication
     path('accounts/register/', views.RegistrationView.as_view(), name="register"),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='account/login.html', authentication_form=LoginForm), name="login"),
     path('accounts/profile/', views.profile, name="profile"),
@@ -16,4 +34,6 @@ urlpatterns = [
     path('accounts/remove-address/<int:id>/', views.remove_address, name="remove-address"),
    path('accounts/logout/', views.LogoutPage,name="logout"),
 
+
+    
 ]
