e1017c0ffa953c3cd65cf819bd18cae72b1ad7d7
esear <arjunanvijay21@gmail.com>
2024-02-02 15:44:59 +0530

    make a router url in user sign up and login page

--- store/urls.py
+++ store/urls.py
@@ -8,4 +8,10 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.home, name="home"),
+     path('accounts/register/', views.RegistrationView.as_view(), name="register"),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='account/login.html', authentication_form=LoginForm), name="login"),
+   
+    path('accounts/add-address/', views.AddressView.as_view(), name="add-address"),
+    
+   path('accounts/logout/', views.LogoutPage,name="logout"),
 ]
