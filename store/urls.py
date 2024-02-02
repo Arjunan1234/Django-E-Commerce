79078ced2f2d1a5c8663289d8a358326b74552b5
esear <arjunanvijay21@gmail.com>
2024-02-02 15:50:02 +0530

    all user related urls.py created

--- store/urls.py
+++ store/urls.py
@@ -8,10 +8,12 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.home, name="home"),
-     path('accounts/register/', views.RegistrationView.as_view(), name="register"),
+      # URL for Authentication
+    path('accounts/register/', views.RegistrationView.as_view(), name="register"),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='account/login.html', authentication_form=LoginForm), name="login"),
-   
+    path('accounts/profile/', views.profile, name="profile"),
     path('accounts/add-address/', views.AddressView.as_view(), name="add-address"),
-    
+    path('accounts/remove-address/<int:id>/', views.remove_address, name="remove-address"),
    path('accounts/logout/', views.LogoutPage,name="logout"),
+
 ]
